27cbb4bf82acb56575c9d22617302c2de3622a71
ilis137 <iliyasshahapure1@gmail.com>
2022-09-15 10:10:57 +0530

    initial commit

--- src/main/java/com/practice/SnakeAndLadder.java
+++ src/main/java/com/practice/SnakeAndLadder.java
@@ -5,29 +5,17 @@ import org.apache.logging.log4j.Logger;
 
 public class SnakeAndLadder {
 
-    private static final Logger log = LogManager.getLogger(SnakeAndLadder.class);
+    private static final Logger LOG = LogManager.getLogger(SnakeAndLadder.class);
 
     public static void main(String[] args) {
-    
-    }
-}
 
-
-class Player{
-    private int position=0;
-    private int diceCount=0;
-
-    public int getPosition() {
-        return position;
+        String message = "Hello , World";
+        LOG.debug(message + " Will Be Printed On Debug ");
+        LOG.info(message + " Will Be Printed On Info ");
+        LOG.warn(message + "Will Be Printed On Warn ");
+        LOG.error(message + " Will Be Printed On Error ");
+        LOG.fatal(message + " Will Be Printed On Fatal ");
+        LOG.info(" Appending string : {}. ", message);
+        System.out.println(message);
     }
-    public void setPosition(int position) {
-        this.position = position;
-    }
-    public int getDiceCount() {
-        return diceCount;
-    }
-    public void setDiceCount(int diceCount) {
-        this.diceCount = diceCount;
-    }
-    
 }
